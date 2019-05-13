a7c42c0c44520ca2291cd25f73237c5bb8307019
delvingdeep <deepdoshi@live.in>
2019-05-12 22:58:11 -0700

    add user input for start and end position

--- src/main.cpp
+++ src/main.cpp
@@ -53,14 +53,26 @@ int main(int argc, const char **argv)
     // TODO: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below.
+    float start_x, start_y;
+    float end_x, end_y;
+
+    std::cout << "Enter x for Start position (Range:0-100) : ";
+    std::cin >> start_x;
+    std::cout << "Enter y for Start position (Range:0-100) : ";
+    std::cin >> start_y;
+    std::cout << "Enter x for End position (Range:0-100) : ";
+    std::cin >> end_x;
+    std::cout << "Enter y for End position (Range:0-100) : ";
+    std::cin >> end_y;
+
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Perform search and render results.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
-    std::cout << "Length of the Path: " << route_planner.GetDistance() << "meters. \n";
+    std::cout << std::setprecision(2) << std::fixed << "Length of the Path: " << route_planner.GetDistance() << " meters. \n";
     Render render{model};
 
     auto display = io2d::output_surface{400, 400, io2d::format::argb32, io2d::scaling::none, io2d::refresh_style::fixed, 30};
