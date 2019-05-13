7e8cefc9cbc2e676ca28519e564ba292df7e9a5e
delvingdeep <deepdoshi@live.in>
2019-05-12 20:48:21 -0700

    fill RoutePlanner header and constructor

--- src/route_planner.h
+++ src/route_planner.h
@@ -10,8 +10,11 @@ class RoutePlanner {
   public:
     RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
     // Add public variables or methods declarations here.
+    float Getdistance() { return distance; }
 
   private:
     // Add private variables or methods declarations here.
     RouteModel &m_Model;
+    RouteModel::Node *start_node, *end_node;
+    float distance;
 };
